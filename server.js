0daec506cb79c857c688ceaa593351ae99103afb
Shimberly <kimmy_11_07@hotmail.com>
2019-01-16 06:07:06 -0500

    sadasd

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(permitirCrossDomain);
 
 app.get('/listarUsuarios', (req, res, next) => {
     
-            return res.json({"nombre":"Kimberly Muñoz"});
+            return res.json({"nombre":"Kimberly Muñoz"},{"nombre":"Pepito"});
             
        
 });
